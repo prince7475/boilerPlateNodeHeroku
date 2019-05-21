32df7453b139786c306bdcc4c27f14abbf4daadb
Prince Donkor <prince7475>
2019-05-20 22:29:10 -0500

    unable to connect to database

--- server/app.js
+++ server/app.js
@@ -1,19 +1,37 @@
 var express = require('express');
+const bodyParser = require('body-parser')
 var path = require('path');
 var logger = require('morgan');
-
 var usersRouter = require('./routes/users');
+const Sequelize = require('sequelize')
 
+// Creating express app
 var app = express();
 
-
-
+// Host react app
 app.use(express.static(path.join(__dirname, '../client/build'))); 
 
+
+//Creating routes
 app.use('/users', usersRouter);
 
+
+// Displaying client index html
 app.get('*', (req,res) => {
     res.send(express.static(path.join(__dirname, '../client/build/index.html')))
 })
+
+//Testing Database
+// @ts-ignore
+const db = new Sequelize('boilerplate', 'postgres', 'password1', {
+    host: 'localhost',
+    dialect:'postgres'
+  });
+  
+
+// @ts-ignore
+db.authenticate()
+    .then(() => console.log('Database connected'))
+    .catch(err => console.log(err))
 
 module.exports = app;
